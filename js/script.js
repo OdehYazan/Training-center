f3b5a7aced83ddbc89654f820a0820e0f7df31e4
OdehYazan <>
2021-04-15 11:11:21 +0000

    loop

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 var image = '<img src="https://cdn.pixabay.com/photo/2012/04/23/15/17/number-38465_960_720.png" alt="15" width ="10px">';
 var image2 = '<img src="https://www.volusion.com/blog/content/images/2019/01/The-7-Most-Popular-Products-You-re-NOT-Allowed-To-Sell-Online.png" alt="not 15" width ="50px">'
 
-
 var age = prompt ('Hallo are you under 15 ? Y or N');
+ while (age !='Y' && age !='N') {
+   var age = prompt ('Hallo are you under 15 ? Y or N');
+ }
+
+
 if (age == 'Y')
 {
-  alert ('Continue');
+  
+  //alert ('Continue');
   document.write (image);
   var uname = prompt ('Enter your name please')
    alert('welcome to GT-center: ' + uname);
   document.write ('     @We hope you will enjoy learning with us@' );
   document.write ('<img src="https://media4.giphy.com/media/fU4elxKlRsulB4Jy7w/200.gif" alt="not 15" "width:="10px">')
+  var nCourse =  prompt ('How many courses you want ?' );
+while (nCourse <= 0 ){
+  var nCourse =  prompt ('How many courses you want ?' );
+}
+ for (var i=1;i<=nCourse;i++){
+   document.write ('<img src="https://milemir.com/wp-content/uploads/2020/11/courseintroimage.jpg" alt="course" "width:="10px">');
+ }
 } else if (age == 'N')
 {
   alert ('You are not allowed !!');
